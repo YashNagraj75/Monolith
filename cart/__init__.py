9b9bfafd4fd7c2f02f43a06897edcc20bcdb9775
Yash Nagraj <pes1202203845@pesu.pes.edu>
2025-01-20 15:35:49 +0530

    Change from eval to json.loads in order to make it faster

--- cart/__init__.py
+++ cart/__init__.py
@@ -6,35 +6,28 @@ from products import Product
 
 
 class Cart:
-    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
-        self.id = id
+    def __init__(self, cart_id: int, username: str, contents: list[Product], cost: float):
+        self.id = cart_id
         self.username = username
         self.contents = contents
         self.cost = cost
 
-    def load(data):
-        return Cart(data['id'], data['username'], data['contents'], data['cost'])
+    def load(self, data):
+        return Cart(data['cart_id'], data['username'], data['contents'], data['cost'])
 
 
 def get_cart(username: str) -> list:
     cart_details = dao.get_cart(username)
     if cart_details is None:
         return []
-    
+
     items = []
     for cart_detail in cart_details:
-        contents = cart_detail['contents']
-        evaluated_contents = eval(contents)  
-        for content in evaluated_contents:
-            items.append(content)
-    
-    i2 = []
-    for i in items:
-        temp_product = products.get_product(i)
-        i2.append(temp_product)
-    return i2
+        contents = json.loads(cart_detail['contents'])
+        items.extend(contents)
 
-    
+    return [products.get_product(i) for i in items]
+
 
 
 def add_to_cart(username: str, product_id: int):
@@ -44,7 +37,6 @@ def add_to_cart(username: str, product_id: int):
 def remove_from_cart(username: str, product_id: int):
     dao.remove_from_cart(username, product_id)
 
+
 def delete_cart(username: str):
     dao.delete_cart(username)
-
-
